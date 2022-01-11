1a3887c779250dcdff868db74c0601aa6cfb79f5
Andreas Huber <info@ndhbr.de>
2022-01-11 14:56:37 +0100

    Added tests

--- tests/test_print_name.py
+++ tests/test_print_name.py
@@ -1,3 +1,5 @@
+import random
+import string
 import os.path
 
 from loop import print_name
@@ -10,3 +12,15 @@ def test_exists():
 
     err = "Die Datei main.py existiert nicht"
     assert os.path.exists('main.py'), err
+
+# Check for functionality
+def test_print_name(capfd):
+    name = ''.join(random.choices(string.ascii_letters, k=10))
+    print_name(name, 3)
+    out, err = capfd.readouterr()
+
+    err = """Deine Funktion wurde auf einen zufälligen Namen getestet.
+    Überprüfe deine Ausgabe, ob wirklich jedes Zeichen stimmt (mit Groß-
+    und Kleinschreibung)"""
+
+    assert out == f'{name} 0\n{name} 1\n{name} 2\n', err
