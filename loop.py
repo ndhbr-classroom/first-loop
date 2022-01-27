3299650fc1924c214a21e73ea2e7f9b37f0c83d8
Andreas Huber <info@ndhbr.de>
2022-01-27 10:51:06 +0100

    Changed comment

--- loop.py
+++ loop.py
@@ -1,13 +1,14 @@
 # Print your name n times
 def print_name(name, count):
 
-    # TODO: Use a for loop to print your name 5 times 
+    # TODO: Use a for loop to print your name n times 
     # Result should look like this:
     # Andi 0
     # Andi 1
     # Andi 2
     # Andi 3
-    # Andi 4
+    # Andi ...
+    # Andi n
     #
     # Tip: Use the function print() to print your name
     # With f'Hello my name is {name}' you can concatenate strings
