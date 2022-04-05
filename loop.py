03a2b56fe6cef0666f772d589cb68599efddc197
Andreas Huber <info@ndhbr.de>
2022-04-05 18:08:50 +0200

    Fixed indentation

--- loop.py
+++ loop.py
@@ -1,17 +1,17 @@
 # Print your name n times
 def print_name(name, count):
-
-    # TODO: Use a for loop to print your name n times 
-    # Result should look like this:
-    # Andi 0
-    # Andi 1
-    # Andi 2
-    # Andi 3
-    # Andi ...
-    # Andi n
-    #
-    # Tip: Use the function print() to print your name
-    # With f'Hello my name is {name}' you can concatenate strings
-    # with variables
-    
-    return
+	
+	# TODO: Use a for loop to print your name n times 
+	# Result should look like this:
+	# Andi 0
+	# Andi 1
+	# Andi 2
+	# Andi 3
+	# Andi ...
+	# Andi n
+	#
+	# Tip: Use the function print() to print your name
+	# With f'Hello my name is {name}' you can concatenate strings
+	# with variables
+	
+	return
